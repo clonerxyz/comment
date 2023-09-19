d612df6d2bb33a5f904c400b74e50fc6c2de0548
clonerxyz <clonerxyz@gmail.com>
2023-09-19 23:20:13 +0700

    test

--- api/index.php
+++ api/index.php
@@ -10,18 +10,21 @@ if(isset($_POST['namadata']) && isset($_POST['haridan']) && isset($_POST['textda
     $data = $_POST['namadata'] . '-' . $_POST['haridan'] . '-' . $_POST['textdata'] . "\r\n";
     $ret = file_put_contents('/public/data.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
-		header("Location: https://www.dhanidansellywedding.site/");
+		//header("Location: https://www.dhanidansellywedding.site/");
+		echo "fail";
         die('There was an error writing this file');
     }
     else {
         echo "$ret bytes written to file";
-		header("Location: https://www.dhanidansellywedding.site/");
+		//header("Location: https://www.dhanidansellywedding.site/");
 		die('success');
     }
 }
 else {
-   header("Location: https://www.dhanidansellywedding.site/");
+   //header("Location: https://www.dhanidansellywedding.site/");
+   echo "fail";
    die('no post data to process');
 }
+?>
  </body>
  </html> 
