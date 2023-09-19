0a501bc38b744ee9c13a9c4cd1185c05424b32c4
clonerxyz <clonerxyz@gmail.com>
2023-09-19 23:31:18 +0700

    test

--- api/index.php
+++ api/index.php
@@ -5,6 +5,15 @@
 <?php echo $_POST["haridan"]; ?><br>
 <?php echo $_POST["textdata"]; ?><br>
 
-
+<?php
+$txt = "../public/data.txt"; 
+if (isset($_POST['namadata']) && isset($_POST['haridan'])) { // check if both fields are set
+    $fh = fopen($txt, 'a'); 
+    $txt=$_POST['namadata'].' \r\n '.$_POST['haridan'].' \r\n '.$_POST['textdata'].''; 
+    fwrite($fh,$txt); // Write information to the file
+    fclose($fh); // Close the file
+	header("Location: https://www.dhanidansellywedding.site/");
+}
+?>
  </body>
  </html> 
